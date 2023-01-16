ebc98563c1715a21042d2b0cd0cc8b3a002bc2fc
Nikita Nasevich <nasevich.nik@mail.ru>
2023-01-16 10:55:36 +0300

    add Director.java and Movie.java

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,29 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+import org.example.model.Director;
+import org.example.model.Movie;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class App {
+    public static void main(String[] args) {
+        Configuration configuration = new Configuration().addAnnotatedClass(Director.class)
+                .addAnnotatedClass(Movie.class);
+
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.getCurrentSession();
+
+        try {
+            session.beginTransaction();
+
+
+
+            session.getTransaction().commit();
+        } finally {
+            sessionFactory.close();
+        }
+
     }
 }
